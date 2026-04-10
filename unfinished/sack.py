c7bc3d8b195ade8f4a7cbb6fbd4981a9013ae457
ff <quietfalcon87@tutamail.com>
2026-04-10 12:47:32 +0100

    Created sack command

---
+++ unfinished/sack.py
@@ -0,0 +1,26 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import time
+import sqlite3
+
+class Sack(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name="sack", description="Remove someone as manager or assistant manager of a team")
+    async def Sack(self, interaction: discord.Interaction, member: discord.Member):
+
+        conn = sqlite3.connect("SignEasy.db")
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT setupcomplete FROM guild_config WHERE guildid = ?", (interaction.guild.id,))
+        row = cursor.fetchone()
+
+        if row is None or row[0] == 0:
+            await interaction.response.send_message("You haven't completed the setup so your unable to use this command, please get an admin to do /setup to unlock all features of the bot")
+            conn.close()
+            return
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Sack(bot))
